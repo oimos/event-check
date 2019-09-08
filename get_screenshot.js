c3c7963a9c48484b08d08c92c9d0c35af787458c
Takahashi, Shinya | Shinya | COOMD <shinya.takahashi@rakuten.com>
2019-09-08 14:31:53 +0900

    replace with puppeteer-core

--- get_screenshot.js
+++ get_screenshot.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
 const devices = require('puppeteer/DeviceDescriptors');
 const path = require('path');
 const screenshotPath = path.resolve('./img/screenshot/') + '/';
